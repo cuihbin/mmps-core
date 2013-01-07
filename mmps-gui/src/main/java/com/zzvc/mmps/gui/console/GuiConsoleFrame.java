28fe817580d41b08b5862e46cb15b26dadc8e2af
cuihbin <cuihbin@gmail.com>
2013-01-07 23:21:19 +0800

    Remove unused codes

--- mmps-gui/src/main/java/com/zzvc/mmps/gui/console/GuiConsoleFrame.java
+++ mmps-gui/src/main/java/com/zzvc/mmps/gui/console/GuiConsoleFrame.java
@@ -9,14 +9,11 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-import java.io.File;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
@@ -27,9 +24,6 @@ import javax.swing.text.Element;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
-import com.zzvc.mmps.gui.file.FileException;
-import com.zzvc.mmps.gui.file.GuiFileFilter;
-
 /**
  * Swing JFrame for GuiConsole
  * @author CHB
@@ -39,9 +33,6 @@ public class GuiConsoleFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private static final int DEFAULT_MAX_LOG_LINES = 200;
-
-	private static final int CHECK_EXISTS = 1;
-	private static final int CONFIRM_OVERWRITE = 2;
 
 	private static final int DEFAULT_FRAME_WIDTH = 640;
 	private static final int DEFAULT_FRAME_HEIGHT = 480;
@@ -63,8 +54,6 @@ public class GuiConsoleFrame extends JFrame {
 	private SimpleAttributeSet infoLogMessageAttributeSet;
 	private SimpleAttributeSet warnLogMessageAttributeSet;
 	private SimpleAttributeSet errorLogMessageAttributeSet;
-	
-	private JFileChooser fDialog = new JFileChooser();
 	
 	public GuiConsoleFrame(GuiConsole console) {
 		this.console = console;
@@ -132,14 +121,6 @@ public class GuiConsoleFrame extends JFrame {
 		
 		this.setVisible(true);
 	}
-//	
-//	public File selectFileForRead(String fileExtension, String fileFilterDescription) {
-//		return selectFile(fileExtension, fileFilterDescription, CHECK_EXISTS);
-//	}
-//	
-//	public File selectFileForWrite(String fileExtension, String fileFilterDescription) {
-//		return selectFile(fileExtension, fileFilterDescription, CONFIRM_OVERWRITE);
-//	}
 	
 	@Override
 	protected void processWindowEvent(WindowEvent e) {
@@ -164,27 +145,6 @@ public class GuiConsoleFrame extends JFrame {
 		});
 		buttonPanel.add(button);
 	}
-//
-//	private File selectFile(String fileExtension, String fileFilterDescription, int flag) {
-//		fDialog.resetChoosableFileFilters();
-//		fDialog.addChoosableFileFilter(new GuiFileFilter(fileExtension, fileFilterDescription));
-//		int result = fDialog.showOpenDialog(null);
-//		if (result == JFileChooser.APPROVE_OPTION) {
-//			File file = fDialog.getSelectedFile();
-//			if ((flag & CHECK_EXISTS) > 0 && !file.exists()) {
-//				JOptionPane.showMessageDialog(null, console.findText("console.gui.file.notexists", file.getName()));
-//				throw new FileException("File not exists");
-//			}
-//			if ((flag & CONFIRM_OVERWRITE) > 0 && file.exists()) {
-//				if (JOptionPane.showConfirmDialog(null, console.findText("console.gui.file.confirmoverwrite", file.getName()), getTitle(), JOptionPane.OK_CANCEL_OPTION) != JOptionPane.OK_OPTION) {
-//					throw new FileException("File already exists");
-//				}
-//			}
-//			return file;
-//		} else {
-//			throw new FileException("User canceled file selection");
-//		}
-//	}
 	
 	protected void addTrace(final String text) {
 		invokeUpdateLogView(text, traceLogMessageAttributeSet);
