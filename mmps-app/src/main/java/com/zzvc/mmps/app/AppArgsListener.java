9c61cfa24981549b8f174f78273347a8a0aa0c32
cuihbin <cuihbin@gmail.com>
2013-01-15 13:45:37 +0800

    Add javadoc comments

--- mmps-app/src/main/java/com/zzvc/mmps/app/AppArgsListener.java
+++ mmps-app/src/main/java/com/zzvc/mmps/app/AppArgsListener.java
@@ -1,5 +1,11 @@
 package com.zzvc.mmps.app;
 
+/**
+ * Listener to process application arguments
+ * 
+ * @author cuihbin
+ * 
+ */
 public interface AppArgsListener {
 	void processArgs(String[] args);
 }
